aa47739ff0abc9975f65b077d24e6851565bbdd2
Quang Hung <duongquanghung0122@gmail.com>
2025-04-17 23:29:32 +0700

    Wrap App with QueryClientProvider and add Toaster

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './lib/queryClient';
+import { Toaster } from './components/ui/sonner';
 
-createRoot(document.getElementById('root')!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+    <Toaster />
+  </QueryClientProvider>
+);

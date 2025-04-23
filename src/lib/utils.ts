9241bbba79adec429cba821a5e0a74ab96ae011e
Quang Hung <duongquanghung0122@gmail.com>
2025-04-23 22:41:11 +0700

    Adding utils functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { format } from 'date-fns';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatTimestamp = (date: Date) => {
+  return format(new Date(date), 'h:mm a');
+};
+
+export const formatDate = (date: Date): string => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  };
+  return new Date(date).toLocaleDateString('en-US', options);
+};
